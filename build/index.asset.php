cf2b80323d70f3197dcfc989c8896a26a0b659f9
Filip Ilic <ilic.filip@gmail.com>
2025-10-10 09:18:33 +0200

    rename wp block

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ef415bc766ec2ece7a1b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ce375e68084c33fab22b');
