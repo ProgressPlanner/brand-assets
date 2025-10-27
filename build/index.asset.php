f84e333cde2dc91d9e34baaa4ab495128feb951b
Filip Ilic <ilic.filip@gmail.com>
2025-10-27 14:15:07 +0100

    fix textdomain and use global CMYK option

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ce375e68084c33fab22b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '39403d0aed0eca430477');
