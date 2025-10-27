821932a05864dffa6df2e51d488ec6ff7a5cb13f
Filip Ilic <filip@emilia.capital>
2025-10-27 13:57:58 +0000

    migration
    Co-authored-by: Sculptor <sculptor@imbue.com>

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '6616a44ed6b55037f993');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '39403d0aed0eca430477');
