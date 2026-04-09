66f09ba6887568cd0a8efc4d9c8fb37e24bddfc1
Filip Ilic <ilic.filip@gmail.com>
2026-04-09 09:52:48 +0200

    bump block apiVersion
    Signed-off-by: Filip Ilic <ilic.filip@gmail.com>

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '6616a44ed6b55037f993');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'f469be414b91e7c37cc7');
